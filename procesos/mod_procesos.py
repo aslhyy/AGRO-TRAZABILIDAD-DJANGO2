4bb89b6d82d8e984f280447918e523dab143fddc
neyi <neyirethdayanasorianoruiz@gmail.com>
2025-11-26 01:10:41 -0500

    Arreglo errores

--- procesos/mod_procesos.py
+++ procesos/mod_procesos.py
@@ -1,6 +1,5 @@
 # procesos/mod_procesos.py
 from django.db import models
-from rest_framework import serializers, generics
 
 # MODELO (con FK tal como pediste: referencia a lotes.Lote)
 class Proceso(models.Model):
@@ -11,18 +10,3 @@ class Proceso(models.Model):
 
     def __str__(self):
         return f"{self.tipo} - Lote {self.lote_id}"
-
-# SERIALIZER
-class ProcesoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Proceso
-        fields = "__all__"
-
-# VISTAS (clases que importará config/urls.py)
-class ProcesoListCreateView(generics.ListCreateAPIView):
-    queryset = Proceso.objects.all()
-    serializer_class = ProcesoSerializer
-
-class ProcesoRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Proceso.objects.all()
-    serializer_class = ProcesoSerializer
